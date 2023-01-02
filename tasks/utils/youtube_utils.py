24d35620ec09398c798bda46739e3f721ce606cb
Akash <akashk@onefin.in>
2023-01-03 01:40:56 +0530

    updating last run time after loading the data into the db

--- tasks/utils/youtube_utils.py
+++ tasks/utils/youtube_utils.py
@@ -5,9 +5,15 @@ from datetime import datetime, timedelta
 YOUTUBE_API_URL = 'https://youtube.googleapis.com/youtube/v3/search'
 SUBJECT = 'football'
 
+def update_last_run_time(time):
+    db = get_db()
+    config = get_configs()
+
+    config['last_run'] = time
+    db.configs.update_one({'_id': config['_id']}, {'$set': config}, upsert=False)
+
 def get_the_start_time():
     """Return the last job execution date."""
-    db = get_db()
     config = get_configs()
     start_from_date = config['last_run']
     current_time = datetime.utcnow()
@@ -15,16 +21,11 @@ def get_the_start_time():
     if start_from_date == '':
         start_from_date = current_time - timedelta(days=10)
     
-    config['last_run'] = current_time
-    db.configs.update_one({'_id': config['_id']}, {'$set': config}, upsert=False)
-
     start_from_date_str = start_from_date.isoformat('T') + 'Z'
-    return start_from_date_str
+    return start_from_date_str, current_time
 
-def fetch_page(published_after, page=''):
+def fetch_page(published_after, api_keys, page=''):
     """Fetch a single page of the youtube data for the given startdate."""
-    api_keys = get_api_keys()
-
     if len(api_keys) == 0:
         raise Exception('No youtube api keys found, please add youtube api keys in the configs collection.')
 
@@ -56,19 +57,21 @@ def fetch_page(published_after, page=''):
 
 def extract_youtube_data():
     """Fetch youtube data for the given startdate."""
-    published_after = get_the_start_time()
+    api_keys = get_api_keys()
+    published_after, current_time = get_the_start_time()
 
     page = ''
     total_videos = []
 
     # Keep on fetching all the pages, until there is no next page left
     while True:
-        videos, next_page = fetch_page(published_after, page)
+        videos, next_page = fetch_page(published_after, api_keys, page)
         print("fetched ", len(videos))
 
         total_videos += videos
 
         if next_page == '':
+            update_last_run_time(current_time)
             return total_videos
         else:
             page = next_page
